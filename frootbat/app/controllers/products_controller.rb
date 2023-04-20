242ed89f11170b6c8b1449586d21d363772f8065
Lukas Eichler <lukas.eichler@yahoo.com>
2023-04-20 22:31:05 +1000

    finished products controller

--- frootbat/app/controllers/products_controller.rb
+++ frootbat/app/controllers/products_controller.rb
@@ -20,7 +20,37 @@ class ProductsController < ApplicationController
         render 'new'
       end
     end
-  
+    
+    def edit
+        @product = Product.find(params[:id])
+      end
+    
+      def update
+        @product = Product.find(params[:id])
+    
+        if @product.update(product_params)
+          redirect_to products_path
+        else
+          render 'edit'
+        end
+      end
+    
+      def destroy
+        @product = Product.find(params[:id])
+        @product.destroy
+    
+        redirect_to products_path
+      end
+    
+      def search
+        @products = Product.where("name ILIKE ?", "%#{params[:q]}%")
+      end
+    
+      private
+    
+      def product_params
+        params.require(:product).permit(:name, :image, :price, :available, :description, :category, :brand, :gtin)
+      end
   
 
   end
